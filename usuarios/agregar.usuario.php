9be78f4bfb6e18c2b7eec43df43b546749978eb0
Santiago Prieto Avila <santiagoprietoavila943@gmail.co>
2024-09-13 21:11:29 -0500

    validaciones usuario

--- usuarios/agregar.usuario.php
+++ usuarios/agregar.usuario.php
@@ -47,63 +47,84 @@
 </head>
 
 <body>
-		<header>
-            <nav class="navbar navbar-expand-lg navbar-primary bg-info">
-                <div class="container-fluid">
-                    <!-- Alinea el título a la izquierda -->
-                    <a class="navbar-brand px-2 text-white" href="../index.administrador.php">Siglo del Hombre</a>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Alinea los elementos del menú a la izquierda utilizando "mr-auto" -->
-                        <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link text-white" href="usuario.php">Usuarios</a>
-                            </li>
-                        </ul>
-                    </div>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-primary bg-info">
+            <div class="container-fluid">
+                <!-- Alinea el título a la izquierda -->
+                <a class="navbar-brand px-2 text-white" href="../index.administrador.php">Siglo del Hombre</a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Alinea los elementos del menú a la izquierda utilizando "mr-auto" -->
+                    <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="usuario.php">Usuarios</a>
+                        </li>
+                    </ul>
                 </div>
-            </nav>
-        </header>
+            </div>
+        </nav>
+    </header>
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-6">
-			<?php
+                <?php
 
-			require '../conexion.php';
+                require '../conexion.php';
 
-			$mensaje = '';
+                $mensaje = '';
 
-			if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-				$correo = $_POST['correo'];
-				$direccion = $_POST['direccion'];
-				$nombre = $_POST['nombre'];
-				$celular = $_POST['celular'];
-				$password = $_POST['password'];
-				$id_tipo = $_POST['id_tipo'];
+                    $correo = $_POST['correo'];
 
-				if ($id_tipo == 'Administrador'){
-					$id_tipo = 1;
-				}else {
-					$id_tipo = 2;
-				}
+                    $match_correo = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
+                    if (!preg_match($match_correo, $correo)) {
+                        echo "<script> alert('Correo electrónico incorrecto.');window.location='agregar.usuario.php' </script>";
+                        exit();
+                    }
+
+                    $direccion = $_POST['direccion'];
+                    $nombre = $_POST['nombre'];
+
+                    $match_correo = '/\d/';
+                    if (preg_match($match_correo, $nombre)) {
+                        echo "<script> alert('Nombre agregado con caracteres incorrectos, no pueden usarse numeros ni caracteres especiales.');window.location='agregar.usuario.php' </script>";
+                        exit();
+                    }
+
+                    $celular = $_POST['celular'];
+
+                    $match_celular = '/^3\d{9}$/';
+                    if (!preg_match($match_celular, $celular)) {
+                        echo "<script> alert('Número de celular incorrecto. Debe comenzar con 3 y tener 10 dígitos.');window.location='agregar.usuario.php' </script>";
+                        exit();
+                    }
+
+                    $password = $_POST['password'];
+                    $id_tipo = $_POST['id_tipo'];
+
+                    if ($id_tipo == 'Administrador') {
+                        $id_tipo = 1;
+                    } else {
+                        $id_tipo = 2;
+                    }
 
 
-				$sql = "INSERT INTO usuario (correo, direccion, nombre, celular, password, rol, id_tipo)
+                    $sql = "INSERT INTO usuario (correo, direccion, nombre, celular, password, rol, id_tipo)
 						VALUES ('$correo', '$direccion', '$nombre', '$celular', '$password', $id_tipo, $id_tipo)";
 
-				if ($mysqli->query($sql) === TRUE) {
-					echo "<div class='alert alert-success'>Usuario agregado correctamente.</div>";
-					echo "<a href='usuario.php' class='btn btn-primary'>Volver a la lista de usuario</a>";
-					exit();
-				} else {
-					echo "Error al guardar el usuario: " . $mysqli->error;
-				}
+                    if ($mysqli->query($sql) === TRUE) {
+                        echo "<div class='alert alert-success'>Usuario agregado correctamente.</div>";
+                        echo "<a href='usuario.php' class='btn btn-primary'>Volver a la lista de usuario</a>";
+                        exit();
+                    } else {
+                        echo "Error al guardar el usuario: " . $mysqli->error;
+                    }
 
-				$mysqli->close();
-			}
-			?>
+                    $mysqli->close();
+                }
+                ?>
                 <div class="registro-caja">
-					
+
                     <h2 class="mt-4">Registro de Usuario</h2>
                     <form method="POST">
                         <div class="form-group">
@@ -122,7 +143,7 @@
                             <label for="password">Contraseña:</label>
                             <input type="password" class="form-control" id="password" name="password" required>
                         </div>
-						<div class="form-group">
+                        <div class="form-group">
                             <label for="password">Celular</label>
                             <input type="text" class="form-control" id="password" name="celular" required>
                         </div>
